4f0c7ccd6308b3b79812957bdf7e442e03b9c1e7
Oliver Myerscough <omm11@temp2.doc.ic.ac.uk>
2015-02-27 13:50:38 +0000

    implemented advanced query recognition feature

--- src/main/java/ic/doc/QueryProcessor.java
+++ src/main/java/ic/doc/QueryProcessor.java
@@ -6,6 +6,9 @@ public class QueryProcessor {
         if (query.contains("Imperial")) {
             return "Imperial is a university in London.";
         }
+        if (query.contains("Hi!")) {
+            return "Hello!";
+        }
         return "";
     }
 }
