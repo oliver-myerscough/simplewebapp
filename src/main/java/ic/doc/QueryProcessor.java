3c38fdf63c2f1e0bb8cd6f3a5d3497257e7d77b6
Oliver Myerscough <oliver.myerscough11@imperial.ac.uk>
2015-03-09 10:26:03 +0000

    merge

--- src/main/java/ic/doc/QueryProcessor.java
+++ src/main/java/ic/doc/QueryProcessor.java
@@ -136,8 +136,6 @@ public class QueryProcessor {
 
 
             int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
 
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
