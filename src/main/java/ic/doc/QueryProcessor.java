06b9ba797ea2f20a33c14baff871ce348cf893aa
Oliver Myerscough <oliver.myerscough11@imperial.ac.uk>
2015-03-09 09:21:50 +0000

    trying to add numbers

--- src/main/java/ic/doc/QueryProcessor.java
+++ src/main/java/ic/doc/QueryProcessor.java
@@ -1,6 +1,11 @@
 package ic.doc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class QueryProcessor {
+
+    Pattern sum = Pattern.compile("what is ([0-9]+) plus ([0-9]+)");
 
     public String process(String query) {
         if (query.contains("Imperial")) {
@@ -11,10 +16,23 @@ public class QueryProcessor {
         }
         if (query.contains("Bye!")) {
             return "Goodbye!";
-        }
+        }   
         if (query.contains("Does it deploy?")) {
             return "Let's see!";
         }
+
+        Matcher m = sum.matcher(query);
+        if (m.matches()) {
+
+            int a = Integer.parseInt(m.group(1));
+            int b = Integer.parseInt(m.group(2));
+
+            return a + b + "";
+
+        }
+
+
+
         return "";
     }
 }
