92df0b6589ac20ca7153c29ea82d2109af975945
Briony Goldsack <briony.goldsack11@imperial.ac.uk>
2015-03-09 10:10:34 +0000

    banana and eiffel tower

--- src/main/java/ic/doc/QueryProcessor.java
+++ src/main/java/ic/doc/QueryProcessor.java
@@ -16,9 +16,6 @@ public class QueryProcessor {
 
     public String process(String query) {
 
-
-
-
         if (query.contains("Imperial")) {
             return "Imperial is a university in London.";
         }
@@ -31,7 +28,17 @@ public class QueryProcessor {
         if (query.contains("Does it deploy?")) {
             return "Let's see!";
         }
-
+        
+        if(query.contains("banana")) {
+        	return "yellow";
+        }
+        
+        if(query.contains("Eiffel tower")) {
+        	return "Paris";
+        }
+        
+        
+       
         Matcher m = sum.matcher(query);
         if (m.find()) {
 
